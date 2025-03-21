b3e3a493f3408bd5c01c03592c647ce6a531860e
高橋智輝 <to-takahashi@kentem.co.jp>
2025-03-21 13:23:32 +0900

    通知を受けとった後に点群を保持する処理を実装

--- Assets/Scripts/PointCloudScanner.cs
+++ Assets/Scripts/PointCloudScanner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
@@ -5,6 +6,8 @@ public class PointCloudScanner : MonoBehaviour
 {
     [SerializeField]
     private ARPointCloudManager pointCloudManager;
+
+    private Dictionary<ulong, Vector3> points = new Dictionary<ulong, Vector3>();
 
     private void Awake()
     {
@@ -16,11 +19,50 @@ public class PointCloudScanner : MonoBehaviour
     {
         // スキャンを開始する
         pointCloudManager.enabled = true;
+
+        // ARPointCloudManagerの更新イベントを登録する
+        pointCloudManager.trackablesChanged.AddListener(OnTrackablesChanged);
     }
 
     public void StopScan()
     {
+        // ARPointCloudManagerの更新イベントを解除する
+        pointCloudManager.trackablesChanged.RemoveListener(OnTrackablesChanged);
+
         // スキャンを停止する
         pointCloudManager.enabled = false;
+    }
+
+    private void OnTrackablesChanged(ARTrackablesChangedEventArgs<ARPointCloud> args)
+    {
+        // 更新されたポイントクラウドの位置情報を取得する
+        foreach (var pointCloud in args.updated)
+        {
+            if(!pointCloud.positions.HasValue)
+            {
+                continue;
+            }
+
+            if(!pointCloud.identifiers.HasValue)
+            {
+                continue;
+            }
+
+            // ポイントクラウドの位置情報をDictionaryに保存する
+            for (int i = 0; i < pointCloud.positions.Value.Length; i++)
+            {
+                var id = pointCloud.identifiers.Value[i];
+                var position = pointCloud.positions.Value[i];
+
+                if (points.ContainsKey(id))
+                {
+                    points[id] = position;
+                }
+                else
+                {
+                    points.Add(id, position);
+                }
+            }
+        }
     }
 }
