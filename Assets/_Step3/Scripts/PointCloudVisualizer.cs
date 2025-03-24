1f2f1004a6a6cac045b822a9d30c496c9a0f06f3
高橋智輝 <to-takahashi@kentem.co.jp>
2025-03-24 16:28:52 +0900

    Step3のViewSceneのスクリプトを追加

---
+++ Assets/_Step3/Scripts/PointCloudVisualizer.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Step3
+{
+    public class PointCloudVisualizer : MonoBehaviour
+    {
+        [SerializeField]
+        private PointCloudFileReader pointCloudFileReader;
+
+        private ParticleSystem.Particle[] particles;
+
+        private void Awake()
+        {
+            pointCloudFileReader.ReadedFile += DisplayPointCloud;
+        }
+
+        private void OnDestroy()
+        {
+            pointCloudFileReader.ReadedFile -= DisplayPointCloud;
+        }
+
+        private void DisplayPointCloud()
+        {
+            // パーティクルシステムを取得する
+            var particleSystem = GetComponent<ParticleSystem>();
+
+            var points = pointCloudFileReader.Points;
+            var numParticles = points.Length;
+
+            // パーティクルの配列を作成して点群の位置情報を設定する
+            particles = new ParticleSystem.Particle[numParticles];
+            for (var i = 0; i < numParticles; i++)
+            {
+                particles[i].startColor = particleSystem.main.startColor.color;
+                particles[i].startSize = particleSystem.main.startSize.constant;
+                particles[i].position = points[i];
+                particles[i].remainingLifetime = 1f;
+            }
+
+            // パーティクルシステムにパーティクルの情報を設定する
+            particleSystem.SetParticles(particles, numParticles);
+        }
+    }
+}
